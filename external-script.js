b3db42fab1e9d6de2277dcb038b63f9606aead80
EternalVelard <chechetkovlad@mail.ru>
2024-03-21 22:14:36 +0300

    modified:   about.html 	modified:   external-script.js 	modified:   footer.css 	modified:   header.css 	modified:   styles.css

--- external-script.js
+++ external-script.js
@@ -1,17 +1,17 @@
 function includeCommonElements() {
-  fetch('header.html')
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById('header-placeholder').innerHTML = data;
-    });
-  fetch('footer.html')
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById('footer-placeholder').innerHTML = data;
-    });
+    fetch('header.html')
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        });
+    fetch('footer.html')
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        });
 }
 
 
 window.onload = function () {
-  includeCommonElements();
+    includeCommonElements();
 };
